0a75072751c396522c0bd5d523ab9910fcef2d00
PARKSEONGJONG <tjdwhd2247@gmail.com>
2019-03-29 13:32:12 +0900

    src add

---
+++ src/main/java/org/seongjong/book/example/MemberService.java
@@ -0,0 +1,14 @@
+package org.seongjong.book.example;
+
+public class MemberService {
+
+	MemberDao memberDao;
+
+	public void setMemberDao(MemberDao memberDao) {
+		this.memberDao = memberDao;
+	}
+
+	public void registerMember() {
+		memberDao.getMember();
+	}
+}
